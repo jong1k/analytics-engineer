f40afc7aaea8d88b27290a8941e7d6441c23052b
jongwon_kim <jongwon_kim@tmax.co.kr>
2022-08-17 21:28:50 +0900

    Initial commit

---
+++ src/main/java/com/linecorp/analyticsengineer/AnalyticsEngineerApplication.java
@@ -0,0 +1,13 @@
+package com.linecorp.analyticsengineer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsEngineerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsEngineerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linecorp/analyticsengineer/AnalyticsEngineerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linecorp.analyticsengineer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsEngineerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
