0bb48f422c4849fb80a9b88c76ed725e1401ddf5
Jongwon <kjw7872@gmail.com>
2022-08-21 23:12:56 +0900

    spring-guides/gs-uploading-files 를 이용하여 파일 업로드 API 구현

--- src/main/java/com/linecorp/analyticsengineer/AnalyticsEngineerApplication.java
+++ src/main/java/com/linecorp/analyticsengineer/AnalyticsEngineerApplication.java
@@ -1,13 +1,25 @@
 package com.linecorp.analyticsengineer;
 
+import com.linecorp.analyticsengineer.service.StorageProperties;
+import com.linecorp.analyticsengineer.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class AnalyticsEngineerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AnalyticsEngineerApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return args -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
